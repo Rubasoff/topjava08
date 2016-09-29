d6d5df6a981af5184901b94a97a504f9f7ba2583
Rubasoff <denisufaltc@gmail.com>
2016-09-29 10:35:36 +0500

    MealTestData

--- src/test/java/ru/javawebinar/topjava/MealTestData.java
+++ src/test/java/ru/javawebinar/topjava/MealTestData.java
@@ -2,13 +2,39 @@ package ru.javawebinar.topjava;
 
 import ru.javawebinar.topjava.matcher.ModelMatcher;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * GKislin
  * 13.03.2015.
  */
 public class MealTestData {
+    public static final List<Meal> USER_MEALS = Arrays.asList(
+            new Meal(LocalDateTime.of(2015, Month.MAY, 30, 10, 0), "Завтрак", 500),
+            new Meal(LocalDateTime.of(2015, Month.MAY, 30, 13, 0), "Обед", 1000),
+            new Meal(LocalDateTime.of(2015, Month.MAY, 30, 20, 0), "Ужин", 500),
+            new Meal(LocalDateTime.of(2015, Month.MAY, 31, 10, 0), "Завтрак", 1000),
+            new Meal(LocalDateTime.of(2015, Month.MAY, 31, 13, 0), "Обед", 500),
+            new Meal(LocalDateTime.of(2015, Month.MAY, 31, 20, 0), "Ужин", 510)
+    );
 
-    public static final ModelMatcher<Meal> MATCHER = new ModelMatcher<>();
+    public static final List<Meal> ADMIN_MEALS = Arrays.asList(
+            new Meal(LocalDateTime.of(2015, Month.MAY, 31, 18, 0), "Ужин", 1500)
+    );
+
+    public static final ModelMatcher<Meal> MATCHER = new ModelMatcher<>(
+            (expected, actual) -> expected == actual ||
+                    (Objects.equals(expected.getDescription(), actual.getDescription())
+                            && Objects.equals(expected.getId(), actual.getId())
+                            && Objects.equals(expected.getCalories(), actual.getCalories())
+                            && Objects.equals(expected.getDateTime(), actual.getDateTime())
+                    )
+    );
 
 }
