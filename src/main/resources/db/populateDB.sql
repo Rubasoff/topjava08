fb315d4493880ffb80f019735febea9b01e7c73f
Rubasoff <denisufaltc@gmail.com>
2016-09-29 09:00:18 +0500

    DB Scripts

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,3 +1,4 @@
+DELETE FROM meals;
 DELETE FROM user_roles;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
@@ -11,3 +12,11 @@ VALUES ('Admin', 'admin@gmail.com', 'admin');
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (dateTime, description, calories) VALUES
+(date '2015-05-30' + time '10:00', 'Завтрак', 500),
+(date '2015-05-30' + time '10:00', 'Обед', 1000),
+(date '2015-05-30' + time '20:00', 'Ужин', 500),
+(date '2015-05-31' + time '10:00', 'Завтрак', 1000),
+(date '2015-05-31' + time '13:00', 'Обед', 500),
+(date '2015-05-31' + time '20:00', 'Ужин', 510);
