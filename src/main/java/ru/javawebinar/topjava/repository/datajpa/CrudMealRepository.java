703671ba7b950abf39b007152d8368fa6c356b28
Rubasoff <denisufaltc@gmail.com>
2016-10-07 11:22:38 +0500

    crud meal repository

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
@@ -1,11 +1,26 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * gkislin
  * 02.10.2016
  */
 public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
+
+    Meal getByIdAndUserId(int id, int userId);
+
+    List<Meal> getAllByUserIdOrderByDateTimeDesc(int userId);
+
+    @Transactional
+    @Modifying
+    int deleteByIdAndUserId(int id,  int userId);
+
+    List<Meal> getAllByUserIdAndDateTimeBetweenOrderByDateTimeDesc(int userId, LocalDateTime startDate, LocalDateTime endDate);
 }
